395573732c1179c8901b6a4efd2272df29ecffd3
azyobuzin <azyobuzin@users.sourceforge.jp>
2019-07-11 13:29:00 +0900

    [WIP] Coordinator

--- PopDEVS/SequentialCoordinators.fs
+++ PopDEVS/SequentialCoordinators.fs
@@ -1,7 +1,32 @@
 module internal PopDEVS.SequentialCoordinators
 
+open System
+open System.Collections.Generic
+
+type Inbox() =
+    let events = List<obj>()
+    let mutable nextTime = 0.0
+
+    member __.AddEvent(event: obj, time: float) =
+        if events.Count > 0 && nextTime <> time then
+            raise (ArgumentException("Unexpected time"))
+        events.Add(event)
+        nextTime <- time
+        ()
+
+    member __.TakeEvents(time: float) =
+        if (time > nextTime) then
+            raise (ArgumentException("Unexpected time"))
+        elif time = nextTime then
+            let ret = events.ToArray() :> IReadOnlyCollection<_>
+            events.Clear()
+            ret
+        else
+            Array.empty :> IReadOnlyCollection<_>
+
 [<AbstractClass>]
 type Coordinator() =
+    abstract member Inbox : event: obj * time: float -> unit
     abstract member CollectOutputs : time: float -> obj seq
     abstract member AdvanceSimulation : time: float -> unit
 
@@ -10,12 +35,15 @@ type CoupledModelSimulator(model: CoupledModel<_, _>) =
     inherit Coordinator()
 
     // TODO: subcoordinators を引数に取らないと……
-    let mutable nextScheduledTime = 0.0
+    let mutable futureEventList = [0.0]
     let mutable lastProcessedTime = 0.0
+
+    override __.Inbox(event, time) =
+        raise (System.NotImplementedException("translation して subcoordinator に転送"))
 
     override __.CollectOutputs(time) =
         raise (System.NotImplementedException())
 
     override __.AdvanceSimulation(time) =
-        assert (time >= lastProcessedTime && time <= nextScheduledTime)
+        assert (time >= lastProcessedTime && time <= List.head futureEventList)
         raise (System.NotImplementedException())
